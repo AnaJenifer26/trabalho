fdbfa11825632794114cd79dd56d75bd3a8d968d
AnaJenifer26 <jeniferana012@gmail.com>
2026-05-13 00:28:44 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,15 @@
+App.java
+
+package app;
+
+import view.TelaHydro;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        TelaHydro tela = new TelaHydro();
+        tela.setVisible(true);
+
+    }
+}
